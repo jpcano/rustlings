e0e9a8207f284094f0fb915eeaf22c5aae24b801
Jesus Cano <jcanovel@gmail.com>
2025-09-27 13:52:28 +0100

    Enums

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::ChangeColor(red, green, blue) => self.change_color(red, green, blue),
+            Message::Echo(message) => self.echo(message),
+            Message::Move(moved_point) => self.move_position(moved_point),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
