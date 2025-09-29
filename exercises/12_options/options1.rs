0239555db0f796a85eb9775045aa6f25fee7fde8
Jesus Cano <jcanovel@gmail.com>
2025-09-29 20:44:29 +0100

    Quiz and Options

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -3,7 +3,13 @@
 // someone eats it all, so no ice cream is left (value 0). Return `None` if
 // `hour_of_day` is higher than 23.
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
-    // TODO: Complete the function body.
+    if hour_of_day < 22 {
+        Some(5)
+    } else if hour_of_day < 24 {
+        Some(0)
+    } else {
+        None
+    }
 }
 
 fn main() {
@@ -20,7 +26,7 @@ mod tests {
         // Option?
         let ice_creams = maybe_ice_cream(12);
 
-        assert_eq!(ice_creams, 5); // Don't change this line.
+        assert_eq!(ice_creams.unwrap(), 5); // Don't change this line.
     }
 
     #[test]
